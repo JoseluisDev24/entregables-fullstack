5e28b702ee37a74115bf2f557861214176a3e30c
JoseluisDev24 <161613858+JoseluisDev24@users.noreply.github.com>
2024-08-11 23:19:10 -0300

    toDo logic creation and modified input element

--- main.js
+++ main.js
@@ -1,21 +1,62 @@
-const todos = [];
+const date = document.getElementById("span-date");
+const DATE = new Date();
+date.innerHTML = DATE.toLocaleDateString("en", {
+  // weekday: "short",
+  month: "short",
+  day: "numeric",
+});
 
-window.onload = () => {
-  const form = document.getElementById("todo-form");
-  form.onsubmit = (e) => {
-    e.preventDefault();
-    const todo = document.getElementById("todo");
-    const todoText = todo.value;
-    todo.value = "";
-    todos.push(todoText);
-    const todoList = document.getElementById("todo-list");
-    todoList.innerHTML = "";
-    for (let i = 0; i < todos.length; i++) {
-      todoList.innerHTML +=
-        "<div class='list-delete'><li class='padding-list'>" +
-        todos[i] +
-        "</li>" +
-        "<a href='#'><img class='imagen' src='/img/delete.png' width='20px'/></a></div>";
+const list = document.querySelector("#list");
+const input = document.querySelector("#input");
+const button = document.querySelector("#button");
+const check = 'fa-check-circle';
+const uncheck = 'fa-circle';
+const lineThrough = 'line-through';
+let id = 0
+
+function addTask(task,id,done,removed) {
+
+  if(removed){
+    return
+  }
+
+  const DONE = done ? check : uncheck;
+  const line = done ? lineThrough : '';
+  
+  const element = `<li id="element">
+                <i class="ico far ${DONE} co" data="done" id='${id}'></i>
+                <p class="${line}text">${task}</p>
+                <i class="ico fas fa-trash de" data="removed" id='${id}'></i>
+              </li>`;
+  list.insertAdjacentHTML("beforeend", element);
+}
+
+button.addEventListener("click", () => {
+  const task = input.value;
+  if (task) {
+    addTask(task,id,false,false);
+  }
+  input.value = "";
+  id++
+});
+
+document.addEventListener("keyup",function (event) {
+  if (event.key === "Enter") {
+    const task = input.value
+    if (task) {
+      addTask(task);
     }
-  };
-};
+    input.value = "";
+  }
+});
+
+// list.addEventListener('click', function (event){
+//   const element = event.target
+//   const elementData = element.attributes.data.value
+//   if(elementData === "done"){
+//     taskDone();
+//     else if(elementData==='removed'){
+// taskRemoved();
+//     }
+//   }
+// })
