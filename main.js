a6a7ba5de007544617a921d05e2f89b36425f762
JoseluisDev24 <161613858+JoseluisDev24@users.noreply.github.com>
2024-08-18 02:58:15 -0300

    logic restructuring for todoList, add delete button and style modification

--- main.js
+++ main.js
@@ -1,62 +1,41 @@
 const date = document.getElementById("span-date");
 const DATE = new Date();
 date.innerHTML = DATE.toLocaleDateString("en", {
-  // weekday: "short",
   month: "short",
   day: "numeric",
 });
 
-const list = document.querySelector("#list");
+const ul = document.getElementById("ul-form");
 const input = document.querySelector("#input");
-const button = document.querySelector("#button");
-const check = 'fa-check-circle';
-const uncheck = 'fa-circle';
-const lineThrough = 'line-through';
-let id = 0
+const addBtn = document.querySelector("#button");
 
-function addTask(task,id,done,removed) {
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-  if(removed){
-    return
-  }
-
-  const DONE = done ? check : uncheck;
-  const line = done ? lineThrough : '';
-  
-  const element = `<li id="element">
-                <i class="ico far ${DONE} co" data="done" id='${id}'></i>
-                <p class="${line}text">${task}</p>
-                <i class="ico fas fa-trash de" data="removed" id='${id}'></i>
-              </li>`;
-  list.insertAdjacentHTML("beforeend", element);
-}
-
-button.addEventListener("click", () => {
   const task = input.value;
-  if (task) {
-    addTask(task,id,false,false);
-  }
-  input.value = "";
-  id++
-});
+  if (task !== "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = task;
 
-document.addEventListener("keyup",function (event) {
-  if (event.key === "Enter") {
-    const task = input.value
-    if (task) {
-      addTask(task);
-    }
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+    li.className = "list-delete";
+
     input.value = "";
   }
 });
 
-// list.addEventListener('click', function (event){
-//   const element = event.target
-//   const elementData = element.attributes.data.value
-//   if(elementData === "done"){
-//     taskDone();
-//     else if(elementData==='removed'){
-// taskRemoved();
-//     }
-//   }
-// })
+function addDeleteBtn() {
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "btn-delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+  });
+  return deleteBtn;
+}
+
